6e0b3b072d200661f43f84dfd2e21e12d13c47e1
liaokylin <kylin@liaokylindeMacBook-Pro.local>
2022-06-19 09:53:46 +0800

    ignore

--- src/pages/common/TabBar.vue
+++ src/pages/common/TabBar.vue
@@ -2,13 +2,13 @@
 <div>
   <router-view/>
   <van-tabbar v-model="active" active-color="#ee0a24">
-    <van-tabbar-item :to="(item.path)"  v-for="item in tabBars" :key="item.id" :icon="item.icon"  @click="onClickTab(item.id)">{{item.name}}</van-tabbar-item>
+    <van-tabbar-item :to="(item.path)"   v-for="item in tabBars" :key="item.id" :icon="item.icon"  @click="onClickTab(item.id)">{{item.name}}</van-tabbar-item>
   </van-tabbar>
 </div>
 </template>
 
 <script>
-import { Tabbar, TabbarItem } from 'vant'
+import {Tabbar, TabbarItem} from 'vant'
 export default {
   name: "TabBar",
   components:{
@@ -34,14 +34,22 @@ export default {
       ]
     }
   },
+  watch:{
+    $route(to) {
+      let active = 0
+       this.tabBars.map((item) => {
+        if (item.path === to.path) {
+            active = item.id
+        }
+      })
+      this.active = active
+    },
+  },
   methods:{
     onClickTab: function (index) {
       const idx = parseInt(index)
-      console.log('tab id',index,this.active)
-      this.active = idx
       let path = this.tabBars[idx].path
       this.$router.push(path)
-
     }
   }
 }
